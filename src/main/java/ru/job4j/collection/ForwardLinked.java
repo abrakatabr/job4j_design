b34e1dccb8ca5e492b77549da2415a2b3abd0753
Pozhar <abrakatabr@mail.ru>
2024-06-17 22:13:19 +0300

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,89 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+
+    private int size;
+    private int modCount;
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> newNode = new Node<T>(value, null);
+        if (size == 0) {
+            head = newNode;
+        } else {
+            Node<T> currentNode = head;
+            int count = 1;
+            while (count < size) {
+                currentNode = currentNode.next;
+                count++;
+            }
+            currentNode.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> getNode = head;
+        for (int i = 0; i < index; i++) {
+            getNode = getNode.next;
+        }
+        return getNode.item;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        T deleted = head.item;
+        head = head.next;
+        size--;
+        return deleted;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            private Node<T> current = head;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                Node<T> result = current;
+                current = current.next;
+                return result.item;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
