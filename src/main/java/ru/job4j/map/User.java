2627e71be474862c2a2738298d721e66c6fafc69
Pozhar <abrakatabr@mail.ru>
2024-06-24 23:02:47 +0300

    USER

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
 
@@ -12,5 +12,32 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar birthdayDate = new GregorianCalendar(1987, 2, 15);
+        User user1 = new User("Alex", 2, birthdayDate);
+        User user2 = new User("Alex", 2, birthdayDate);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.print(map);
     }
 }
