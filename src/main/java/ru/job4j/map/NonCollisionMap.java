1184211c1b59d7f6ba46941b8319fbf96268e005
Pozhar <abrakatabr@mail.ru>
2024-06-24 22:54:24 +0300

    10. Реализовать собственную структуру данных - HashMap [#1008]

--- src/main/java/ru/job4j/map/NonCollisionMap.java
+++ src/main/java/ru/job4j/map/NonCollisionMap.java
@@ -19,7 +19,7 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
 
     @Override
     public boolean put(K key, V value) {
-        if (((float) count / capacity) == LOAD_FACTOR) {
+        if ((count * 100/ capacity) >= LOAD_FACTOR * 100) {
             expand();
         }
         boolean result = false;
@@ -108,9 +108,6 @@ public class NonCollisionMap<K, V> implements SimpleMap<K, V> {
 
             @Override
             public K next() {
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
