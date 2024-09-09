110a6d964353e7bbb78c8055c5b428c50d911530
Pozhar <abrakatabr@mail.ru>
2024-09-09 21:11:06 +0300

    FileInputStream

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ReadFile {
     public static void main(String[] args) {
