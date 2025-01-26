a5b456a8c44fc52a4ed80cb7607b84ae9b12bccc
Pozhar <abrakatabr@mail.ru>
2025-01-26 22:22:10 +0300

    4. Найти утечку памяти [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/PostStore.java
@@ -0,0 +1,28 @@
+package ru.job4j.gc.leak;
+
+import ru.job4j.gc.leak.models.Post;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class PostStore {
+
+    private final Map<Integer, Post> posts = new HashMap<>();
+
+    private final AtomicInteger atomicInteger = new AtomicInteger(1);
+
+    public Post add(Post post) {
+        var id = atomicInteger.getAndIncrement();
+        post.setId(id);
+        posts.put(id, post);
+        return post;
+    }
+
+    public void removeAll() {
+        posts.clear();
+    }
+
+    public  Collection<Post> getPosts() {
+        return posts.values();
+    }
+}
