6833f3f9eef9ad9c9ccef283bc19a0cf05d367ca
Pozhar <abrakatabr@mail.ru>
2025-01-28 15:17:01 +0300

    1. Реализация кеша на SoftReference [#1592]

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,41 @@
+package ru.job4j.cache;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.Optional;
+
+public class DirFileCache extends AbstractCache<String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        validateDir(cachingDir);
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key) {
+        String filePath = cachingDir + "/" + key;
+        validate(filePath);
+        String value = null;
+        try {
+            value = Files.readString(Path.of(filePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
+    private void validate(String key) {
+        if (!Files.exists(Path.of(key))) {
+            throw new IllegalArgumentException("Файл не существует.");
+        }
+    }
+
+    private void validateDir(String cachingDir) {
+        if (!Files.isDirectory(Path.of(cachingDir))) {
+            throw new IllegalArgumentException("Директории не существуетю");
+        }
+    }
+}
